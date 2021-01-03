77389bf5c9beeebc02c2ac0554677b3c6f54f788
谢芃 <949026370@qq.com>
2021-01-03 23:03:45 +0800

    feat

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,78 @@
 <template>
   <div>
-    <div>
+    <div class="img-box">
       <img class="img" src="../assets/word/高考3500词1.jpg" />
+      <div class="shade">
+        <div class="left" v-show="showLeft"></div>
+        <div class="center" v-show="showCenter"></div>
+        <div class="right" v-show="showRight"></div>
+      </div>
     </div>
-    <button class="b-r">默写中文</button>
-    <button>默写英文</button>
-    <div class="shade">
-      <div class="left"></div>
-      <div class="center"></div>
-      <div class="right"></div>
-    </div>
+    <button class="b-r" @click="dictationCN">默写中文</button>
+    <button @click="dictationEN">默写英文</button>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  },
   data() {
     return {
-      count: 0
+      count: 0,
+      showLeft: true,
+      showCenter: false,
+      showRight: true
+    }
+  },
+  methods: {
+    dictationCN() {
+      this.showLeft = false
+      this.showRight = true
+    },
+    dictationEN() {
+      this.showLeft = true
+      this.showRight = false
     }
   }
 }
 </script>
-<style land="scss">
-.img{
+<style lang="scss">
+.img-box {
+  position: relative;
+  margin-bottom: 20px;
+}
+.img {
   width: 1000px;
   height: 563px;
 }
-.b-r{
+.b-r {
   margin-right: 20px;
 }
-.shade{
-  .left{}
-  .center{}
-  .right{}
+.shade {
+  background-color: #fff;
+  .left {
+    position: absolute;
+    top: 0;
+    left:242px;
+    width: 250px;
+    height: 563px;
+    background-color: #fff;
+  }
+  .center {
+    position: absolute;
+    top: 0;
+    left: 490px;
+    width: 210px;
+    height: 563px;
+    background-color: #fff;
+  }
+  .right {
+    position: absolute;
+    top: 0;
+    left: 704px;
+    width: 500px;
+    height: 563px;
+    background-color: #fff;
+  }
 }
 </style>
